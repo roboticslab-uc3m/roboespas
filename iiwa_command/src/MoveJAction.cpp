4ff41a4e4e7bf80de3a5207b17005f9b040fd606
Leytha <anac.jaquete@gmail.com>
2020-05-14 13:20:57 +0200

    Added MoveJ using iiwa_stack

--- iiwa_command/src/MoveJAction.cpp
+++ iiwa_command/src/MoveJAction.cpp
@@ -12,6 +12,8 @@
 #include <Eigen/Dense>
 #include <math.h>
 #include <cmath>
+#include <iiwa_msgs/JointPosition.h>
+#include <iiwa_msgs/TimeToDestination.h>
 
 using namespace std;
 
@@ -26,8 +28,11 @@ class MoveJAction
     //Iiwa gazebo state subscriber
     ros::Subscriber iiwa_state_sub;
     sensor_msgs::JointState joint_state;
-    //Iiwa gazebo command publisher
+    //Iiwa gazebo/fri command publisher
     ros::Publisher iiwa_command_pub;
+    //Iiwa_stack
+    ros::Publisher iiwa_command_position_pub;
+    ros::ServiceClient timeToDestClient;
     //Parameters
     double control_step_size;
     std::string robot_mode;
@@ -70,6 +75,11 @@ class MoveJAction
         {
             iiwa_command_pub = nh.advertise<trajectory_msgs::JointTrajectoryPoint>("/iiwa_gazebo/joint_command", 1000, false);
             iiwa_state_sub = nh.subscribe("/iiwa_gazebo/joint_state", 1000, &MoveJAction::callback_iiwa_gazebo_state, this);
+        }
+        else if (strcmp(robot_mode.c_str(), "iiwa_stack")==0)
+        {
+            iiwa_command_position_pub = nh.advertise<iiwa_msgs::JointPosition>("/iiwa/command/JointPosition", 0);
+	        timeToDestClient=nh.serviceClient<iiwa_msgs::TimeToDestination>("/iiwa/state/timeToDestination");
         }
         else
         {
@@ -116,70 +126,109 @@ class MoveJAction
         {
             ROS_ERROR("Failed to read '/iiwa_command/velocity' on param server, using 0.5");
         }
-        //Calculate qdot_max with that percentage
-        Eigen::VectorXd qdot = qdot_max * velocity;
-        //Calculate maximum joint position increment for each joint, taking into account the control step size and the current maximum velocity
-        // qdot = incq/t -> incq= qdot*t 
-        Eigen::VectorXd qinc_max = qdot*control_step_size;
-        std::cout << "qinc_max: " << qinc_max.transpose() << std::endl;
-        
-        ros::Time tStartTraj = ros::Time::now();
-        bool cont=true;
-        while (cont)
+        //If mode is gazebo or fri, the joint_position should be divided in several near joint_positions
+        if (strcmp(robot_mode.c_str(), "gazebo")==0 || strcmp(robot_mode.c_str(), "fri")==0)
         {
-            sensor_msgs::JointState freezed_joint_state = joint_state;
-            //Get current position;
-            Eigen::VectorXd q_curr = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(freezed_joint_state.position.data(), freezed_joint_state.position.size());
-            //Calculate the difference in radians        
-            Eigen::VectorXd q_diff = q_goal-q_curr;
-            //Check if it is far from the goal position
-            Eigen::VectorXd ratio_far = q_diff.array()/qinc_max.array();
-            double ratio_farest = ratio_far.array().abs().maxCoeff();
-            if (ratio_farest>=1)
+    
+            //Calculate qdot_max with that percentage
+            Eigen::VectorXd qdot = qdot_max * velocity;
+            //Calculate maximum joint position increment for each joint, taking into account the control step size and the current maximum velocity
+            // qdot = incq/t -> incq= qdot*t 
+            Eigen::VectorXd qinc_max = qdot*control_step_size;        
+            ros::Time tStartTraj = ros::Time::now();
+            bool cont=true;
+            while (cont)
             {
-                q_diff = q_diff.array()/ratio_farest;
-            } //else stay as it is
-
-            //Get the next joint position
-            Eigen::VectorXd q_next = q_curr + q_diff;
-            //Check its inside the workspace
-            for (int j=0; j<q_next.size(); j++)
-            {
-                if (std::abs(q_next[j]) >= q_max[j])
+                sensor_msgs::JointState freezed_joint_state = joint_state;
+                //Get current position;
+                Eigen::VectorXd q_curr = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(freezed_joint_state.position.data(), freezed_joint_state.position.size());
+                //Calculate the difference in radians        
+                Eigen::VectorXd q_diff = q_goal-q_curr;
+                //Check if it is far from the goal position
+                Eigen::VectorXd ratio_far = q_diff.array()/qinc_max.array();
+                double ratio_farest = ratio_far.array().abs().maxCoeff();
+                if (ratio_farest>=1)
                 {
-                    ROS_ERROR("Intermediate joint position outside workspace");
-                    as_result.trajectory_joint_state=trajectory_joint_state;
-                    as_result.trajectory_commanded=trajectory_commanded;
-                    as.setSucceeded(as_result);
-                    return;
-                }
-            }
-            //Prepare variables to command
-            trajectory_msgs::JointTrajectoryPoint point_command;
-            for (int i=0; i<q_next.size(); i++)
-            {
-                point_command.positions.push_back(q_next[i]);
-            }
-            ros::Duration time_from_start = ros::Time::now()-tStartTraj;
-            point_command.time_from_start = time_from_start;
+                    q_diff = q_diff.array()/ratio_farest;
+                } //else stay as it is
 
-            //Save point_commanded and joint_state into vectors
-            trajectory_commanded.points.push_back(point_command);
-            trajectory_joint_state.push_back(freezed_joint_state);
-            as_feedback.joint_state = freezed_joint_state;
-            as_feedback.point_commanded = point_command;
-            as_feedback.time_from_start=time_from_start.toSec();
-            as.publishFeedback(as_feedback);
-            //Command it
-            std::cout << "qinc_max: " << qinc_max.transpose() << std::endl;
-            std::cout << "q_inc: " << q_diff.transpose() << std::endl;
-            std::cout << "q_next: " << q_next.transpose() << std::endl;
-            iiwa_command_pub.publish(point_command);
-            cont = q_diff.array().abs().maxCoeff() > error_joint_position_stop;
-            ros::Duration(control_step_size).sleep();
+                //Get the next joint position
+                Eigen::VectorXd q_next = q_curr + q_diff;
+                //Check its inside the workspace
+                for (int j=0; j<q_next.size(); j++)
+                {
+                    if (std::abs(q_next[j]) >= q_max[j])
+                    {
+                        ROS_ERROR("Intermediate joint position outside workspace");
+                        as_result.trajectory_joint_state=trajectory_joint_state;
+                        as_result.trajectory_commanded=trajectory_commanded;
+                        as.setSucceeded(as_result);
+                        return;
+                    }
+                }
+                //Prepare variables to command
+                trajectory_msgs::JointTrajectoryPoint point_command;
+                for (int i=0; i<q_next.size(); i++)
+                {
+                    point_command.positions.push_back(q_next[i]);
+                }
+                ros::Duration time_from_start = ros::Time::now()-tStartTraj;
+                point_command.time_from_start = time_from_start;
+
+                //Save point_commanded and joint_state into vectors
+                trajectory_commanded.points.push_back(point_command);
+                trajectory_joint_state.push_back(freezed_joint_state);
+                as_feedback.joint_state = freezed_joint_state;
+                as_feedback.point_commanded = point_command;
+                as_feedback.time_from_start=time_from_start.toSec();
+                as.publishFeedback(as_feedback);
+                //Command it
+                std::cout << "qinc_max: " << qinc_max.transpose() << std::endl;
+                std::cout << "q_inc: " << q_diff.transpose() << std::endl;
+                std::cout << "q_next: " << q_next.transpose() << std::endl;
+                iiwa_command_pub.publish(point_command);
+                cont = q_diff.array().abs().maxCoeff() > error_joint_position_stop;
+                ros::Duration(control_step_size).sleep();
+            }
+            as_result.trajectory_joint_state=trajectory_joint_state;
+            as_result.trajectory_commanded=trajectory_commanded;
         }
-        as_result.trajectory_joint_state=trajectory_joint_state;
-        as_result.trajectory_commanded=trajectory_commanded;
+        else if(strcmp(robot_mode.c_str(), "iiwa_stack")==0)
+        {
+            //Create the message to send and fill it
+	        iiwa_msgs::JointPosition jPos;
+	        jPos.position.a1=q_goal[0];
+	        jPos.position.a2=q_goal[1];
+	        jPos.position.a3=q_goal[2];
+	        jPos.position.a4=q_goal[3];
+	        jPos.position.a5=q_goal[4];
+	        jPos.position.a6=q_goal[5];
+	        jPos.position.a7=q_goal[6];
+	        iiwa_command_position_pub.publish(jPos);
+	        iiwa_msgs::TimeToDestination timeToDestService;
+	        //Wait until the remaining time to reach that joint position is under 0.1 seconds and higher than -0.1 to work around the problem that makes the service timeToDestination return a really low number the first few times called. The loop is exited if remainingTime=-999, which means there's an error, or the remainingTime is around 0.
+	        /*float remainingTime=1;
+	        while (remainingTime<-0.1 || remainingTime>0.1)
+	        {
+		        //Check the remaining time every iteration
+		        timeToDestClient.call(timeToDestService);
+		        remainingTime=timeToDestService.response.remaining_time;
+		        ros::Duration(0.05).sleep();
+		        if (remainingTime==-999)
+			        break;
+	        }
+	        //If there was an error, return false
+	        if (remainingTime==-999)
+	        {
+		        res.success=false;
+		        ROS_INFO("Error");
+	        }
+	        else
+	        {
+		        res.success=true;
+	        }
+            */ //TODO:Uncomment when connected to iiwa_stack to check it works
+        }
         as.setSucceeded(as_result);
         ROS_INFO("MoveJ action server result sent");
     }
