f3b88486af4b03efb3b020315ee2a15c99580989
Angordil <43574001+Angordil@users.noreply.github.com>
2020-06-30 16:30:20 +0200

    Update f_importCMCExcitations.m

--- matlab/OpenSimControl/MatlabCode/Principales/f_importCMCExcitations.m
+++ matlab/OpenSimControl/MatlabCode/Principales/f_importCMCExcitations.m
@@ -1,10 +1,21 @@
-function [cmcExcitations] = f_importCMCExcitations(controlsSTO)
+function [cmcExcitations] = f_importCMCExcitations(sto)
 % This function saves in a .mat struct the estimated excitations after CMC
 % completion
-    controls = importdata(controlsSTO);
-    for i = 1:length(controls.colheaders)
-        varName = controls.colheaders{i};
-        cmcExcitations.(varName) = controls.data(:,i);
+    results = importdata(sto);
+    for i = 1:length(results.colheaders)
+        varName = results.colheaders{i};
+        if i==1
+            cmcExcitations.(varName) = results.data(:,1);
+        else
+            if contains(sto, 'controls')
+                cmcExcitations.(varName) = results.data(:,i);
+            else
+                if contains(varName, 'forceset') && contains(varName, 'activation')
+                    muscleName = extractBetween(varName,'/forceset/','/activation');
+                    cmcExcitations.(muscleName{1}) = results.data(:,i);
+                end
+            end
+        end
     end
 end
 
