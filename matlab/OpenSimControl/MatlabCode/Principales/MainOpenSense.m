f3d792971385f3a77b745323f92fe5dd229501df
Angordil <43574001+Angordil@users.noreply.github.com>
2020-07-09 16:21:56 +0200

    Update MainOpenSense.m

--- matlab/OpenSimControl/MatlabCode/Principales/MainOpenSense.m
+++ matlab/OpenSimControl/MatlabCode/Principales/MainOpenSense.m
@@ -64,11 +64,11 @@ switch modeladoMuscular
     case 'Sano_Millard'
         MODELO = ['Arm_Flexion_Millard_', version, '.osim'];
 end
-
+% createActuatorsFile([CD_model, '\', MODELO]);
 % Load trial
 IMUpath = strcat(CD_model, '\IMUData');
-load(strcat(IMUpath, '\trialOpenSense.mat'));
-
+load(strcat(IMUpath, '\trialOpenSense_5sensors.mat'));
+   
 %% Create Xsens file from trial
 f_createXsensFile(trial, IMUpath);
 
@@ -81,12 +81,12 @@ xsens = XsensDataReader(xsensSettings);
 % Get a table reference for the data
 tables = xsens.read(strcat(IMUpath, '/'));
 % get the trial name from the settings
-trial = char(xsensSettings.get_trial_prefix());
+trialName = char(xsensSettings.get_trial_prefix());
 
 % Get Orientation Data as quaternions
 quatTable = xsens.getOrientationsTable(tables);
 % Write to file
-STOFileAdapterQuaternion.write(quatTable,  [IMUpath, '\', trial '_orientations.sto']);
+STOFileAdapterQuaternion.write(quatTable,  [IMUpath, '\', trialName '_orientations.sto']);
 
 %% Calibrate OpenSim model
 % Set variables to use
@@ -115,13 +115,13 @@ model = imuPlacer.getCalibratedModel();
 
 % Print the calibrated model to file.
 model.print(strrep(modeloPath, '.osim', '_calibrated.osim') );
+MODELO = strrep(MODELO, '.osim', '_calibrated.osim');
+
 %% Obtain IK motion from orientation tracking
 % Set variables to use
-% modeloPath = [CD_model, '\Arm_Flexion_Millard_V2_calibrated.osim']; %'Rajagopal_2015_calibrated.osim';                % The path to an input model
-MODELO = strrep(MODELO, '.osim', '_calibrated.osim');
 visualizeTracking = false;  % Boolean to Visualize the tracking simulation
-startTime = 22; %7.25;          % Start time (in seconds) of the tracking simulation. 
-endTime = 27; %15;              % End time (in seconds) of the tracking simulation.
+startTime = 5; %7.25;          % Start time (in seconds) of the tracking simulation. 
+endTime = 12.5; %15;              % End time (in seconds) of the tracking simulation.
 resultsDirectory = [CD_model,'\IKResults'];
 
 % Instantiate an InverseKinematicsTool
@@ -141,6 +141,139 @@ imuIK.run(visualizeTracking);
 
 OutputMotionStr = 'ik_PruebaFlexExt_orientations.mot';
 motFilePath=strcat(CD_model,'\IKResults\',OutputMotionStr);
+
+%% Create External Loads
+t = trial.DelsysSensors.Trial.Sensor1.IMU.Timestamps;
+ForceAndTorque = zeros(length(t),9);
+ForceAndTorque(:,2) = ones(length(ForceAndTorque),1).*(-2.0);
+ForceAndTorque(:,4)= 0;
+ForceAndTorque(:,5)= -0.08; %posicion de donde se aplica la fuerza respecto al s.c de la mano
+ForceAndTorque(:,6)= 0;
+% Guardar las ExternalLoads en una variable de OpenSim    
+%Guardo todos los datos en un Storage -> ExternalForcesStorage
+ExternalForcesTorquesStorage=org.opensim.modeling.Storage();
+ExternalForcesTorquesStorage.setName('ExternalLoads.mot');   %
+ExternalForcesTorquesStorage.setInDegrees(true); %Igual que en Gait2354
+% Time=TrcTableCreada.getIndependentColumn;
+%     Force=ForceAndTorque(:,1:3);
+%     Torque=ForceAndTorque(:,7:9);
+ColumnLabels=org.opensim.modeling.ArrayStr();
+ColumnLabels.append('time');
+ColumnLabels.append('hand_force_vx');
+ColumnLabels.append('hand_force_vy');
+ColumnLabels.append('hand_force_vz');
+ColumnLabels.append('hand_force_px');
+ColumnLabels.append('hand_force_py');
+ColumnLabels.append('hand_force_pz');
+ColumnLabels.append('hand_torque_x');
+ColumnLabels.append('hand_torque_y');
+ColumnLabels.append('hand_torque_z');
+ExternalForcesTorquesStorage.setColumnLabels(ColumnLabels);
+%Meto los valores COMO STATEVECTORS
+for i=1:length(t(1,:))
+    fila=org.opensim.modeling.StateVector();
+    v=org.opensim.modeling.Vector();
+    v.resize(int16(length(ForceAndTorque(1,:))));
+    for j = 1:length(ForceAndTorque(1,:))
+        v.set(int16(j-1),ForceAndTorque(i,j)); %Vector empieza desde 0
+        fila.setStates(1,v);
+        fila.setTime(t(i));
+        %             size=fila.getSize;
+    end
+    ExternalForcesTorquesStorage.append(fila);
+end
+ExternalForcesTorquesStorage.print(strcat(CD_model,'\ForcesAndTorques\ExternalLoads.mot'));
+External_Force_FT=org.opensim.modeling.ExternalForce(ExternalForcesTorquesStorage,'hand_force_v','hand_force_p','hand_torque_','hand','ground','hand'); 
+External_Force_FT.setName('TCP_ExternalForce');
+External_Force_FT.setAppliedToBodyName('hand');        %%%%%
+% External_Force.setForceExpressedInBodyName('hand');  % No aplica en
+% el caso de que la fuerza se aplique sobre un body (Instrucciones de
+% OpenSim)
+External_Force_FT.print(strcat(CD_model,'\ForcesAndTorques\ExternalForce.xml'));
+clear ColumnLabels fila v
+External_Loads_FT=org.opensim.modeling.ExternalLoads();
+External_Loads_FT.adoptAndAppend(External_Force_FT);
+External_Loads_FT.setLowpassCutoffFrequencyForLoadKinematics(6);
+External_Loads_FT.setDataFileName('ExternalLoads.mot');
+% External_Loads_FT.setExternalLoadsModelKinematicsFileName(motFilePath); NO ES NECESARIO
+xmlExternalLoadsFileName_FT=strcat(CD_model,'\ForcesAndTorques\ExternalLoads.xml');
+External_Loads_FT.print(xmlExternalLoadsFileName_FT);
+motExternalLoadsFileName_FT=strcat(CD_model,'\ForcesAndTorques\ExternalLoads.mot');
+    
+%% RRA - PENDIENTE DE VALIDAR
+import org.opensim.modeling.*
+CD_rra=strcat(CD_model,'\RRA');
+%Coordenadas (todas)
+CoordSet= model.getCoordinateSet();
+elv_angle=CoordSet.get('elv_angle');
+shoulder_elv=CoordSet.get('shoulder_elv');
+shoulder_rot=CoordSet.get('shoulder_rot');
+elbow_flexion=CoordSet.get('elbow_flexion');
+pro_sup=CoordSet.get('pro_sup');
+deviation=CoordSet.get('deviation');
+flexion=CoordSet.get('flexion');
+%Bloqueos:
+elv_angle.set_locked(0);
+shoulder_elv.set_locked(0);
+shoulder_rot.set_locked(0);
+elbow_flexion.set_locked(0);
+pro_sup.set_locked(1);
+deviation.set_locked(1);
+flexion.set_locked(1);
+model.print([CD_model,'\',MODELO]);
+
+sto=org.opensim.modeling.Storage(motFilePath);
+% Tiempos:
+StartTime=sto.getFirstTime;
+LastTime=sto.getLastTime;
+
+rraTool=RRATool();
+rraTool.setName('RRATool');
+% Model
+rraTool.setModel(model);
+rraTool.setModelFilename(strcat(CD_model,'\',MODELO));
+% Replace Force Set
+rraTool.setReplaceForceSet(true);
+% Set Force Set
+actuatorsFile = [CD_rra, '\RRA_Actuators.xml'];
+forceSetFile = org.opensim.modeling.ArrayStr();
+forceSetFile.append(actuatorsFile);
+rraTool.setForceSetFiles(forceSetFile);
+% Results
+rraTool.setResultsDir(strcat(CD_model,'\RRAResults'));
+% Output precision
+rraTool.setOutputPrecision(20);
+% Time Range
+rraTool.setStartTime(StartTime);
+rraTool.setFinalTime(LastTime);
+% Solve For Equilibrium
+rraTool.setSolveForEquilibrium(false);
+% External Loads File
+rraTool.setExternalLoadsFileName(xmlExternalLoadsFileName_FT);
+% Filtered Motion
+rraTool.setDesiredKinematicsFileName(motFilePath);
+% Tasks
+rraTool.setTaskSetFileName(strcat(CD_rra,'\RRA_Tasks-ROBOESPAS_flex.xml'));
+% Constraints
+rraTool.setConstraintsFileName(strcat(CD_rra,'\RRA_ControlConstraints_ROBOESPAS2.xml'));
+%  Low Pass Frequency
+rraTool.setLowpassCutoffFrequency(6);  %Se queja luego si no lo meto
+% Output model Name
+rraTool.setOutputModelFileName(strcat(CD_model,'\',strrep(MODELO, '_calibrated.osim', '_adjusted.osim')));
+%Actuators
+%     rraTool.setForceSetFiles(strcat(CD_rra,'Reserve_Actuators_Roboespas.xml')); %%%%%%%
+
+rraTool.setAdjustedCOMBody('thorax');
+rraTool.setAdjustCOMToReduceResiduals(1);
+
+rraTool.print(strcat(CD_rra,'\setupActualRRA.xml'));
+
+rra = RRATool(strcat(CD_rra,'\setupActualRRA.xml'));
+rra.run; %Esto se hace por seguridad. Recomendaciï¿½n de OpenSim.
+% rraTool.run
+disp('RRA done');
+
+MODELO = strrep(MODELO, '_calibrated.osim', '_adjusted.osim');
 %% CMC
 import org.opensim.modeling.*;
 CD_cmc=strcat(CD_model,'\CMC');
@@ -153,7 +286,6 @@ elbow_flexion=CoordSet.get('elbow_flexion');
 pro_sup=CoordSet.get('pro_sup');
 deviation=CoordSet.get('deviation');
 flexion=CoordSet.get('flexion');
-r_z = CoordSet.get('r_z');
 %Bloqueos:
 elv_angle.set_locked(0);
 shoulder_elv.set_locked(0);
@@ -162,15 +294,24 @@ elbow_flexion.set_locked(0);
 pro_sup.set_locked(1);
 deviation.set_locked(1);
 flexion.set_locked(1);
-r_z.set_locked(1);
-model.print(MODELO);
+model.print([CD_model,'\',MODELO]);
 
 sto=org.opensim.modeling.Storage(motFilePath);
 % Tiempos:
 StartTime=sto.getFirstTime;
 LastTime=sto.getLastTime;
 
-
+% add reserve actuators and residuals
+import org.opensim.modeling.*;
+reserve_actuators = [CD_cmc, '\CMC_Actuators.xml'];
+% reserve_actuators = [CD_model, '\Arm_Flexion_Millard_V2_Actuators.xml'];
+force_set = org.opensim.modeling.ForceSet(reserve_actuators, true);
+force_set.setMemoryOwner(false);  % model will be the owner
+for i = 1:force_set.getSize()-1
+    model.updForceSet().append(force_set.get(i));
+end
+model.print([CD_model,'\',MODELO]);
+   
 %     cmcTool=CMCTool(strcat(CD_cmc,"\CMC_Setup_Roboespas_Flex.xml"));
 cmcTool=CMCTool();
 % Name
@@ -180,6 +321,8 @@ cmcTool.setModel(model);
 cmcTool.setModelFilename(strcat(CD_model,'\',MODELO));
 % Replace Force Set
 cmcTool.setReplaceForceSet(false);
+% Reserve actuators
+% cmcTool.setForceSetFiles(strcat(CD_cmc,'\CMC_Reserve_Actuators.xml'));
 % Results
 cmcTool.setResultsDir(strcat(CD_model,'\CMCResults'));
 % Output precision
@@ -198,13 +341,13 @@ cmcTool.setMinDT(1e-03);
 % Integrator error tolerance
 cmcTool.setErrorTolerance(1e-04);
 % External Loads File
-% cmcTool.setExternalLoadsFileName(xmlExternalLoadsFileName_FT);
+cmcTool.setExternalLoadsFileName(xmlExternalLoadsFileName_FT);
 % Filtered Motion
 cmcTool.setDesiredKinematicsFileName(motFilePath);
 % Tasks
 cmcTool.setTaskSetFileName(strcat(CD_cmc,'\CMC_Tasks-ROBOESPAS_flex.xml'));
 % Actuator Constraints
-cmcTool.setConstraintsFileName(strcat(CD_cmc,'\CMC_ControlConstraints_ROBOESPAS.xml'));
+cmcTool.setConstraintsFileName(strcat(CD_cmc,'\CMC_ControlConstraints_ROBOESPAS2.xml'));
 %  Low Pass Frequency
 cmcTool.setLowpassCutoffFrequency(6);
 % Look ahead window time
@@ -231,17 +374,21 @@ disp('CMC Completado');
 
 %% Representar excitaciones estimadas con CMC
 [cmcExcitations] = f_importCMCExcitations(strcat(CD_cmc, 'Results\CMCtool_controls.sto'));
-[cmcActivations] = f_importCMCExcitations(strcat(CD_cmc, 'Results\CMCtool_states.sto'));
+[cmcActivations,joints] = f_importCMCExcitations(strcat(CD_cmc, 'Results\CMCtool_states.sto'));
 muscles = fieldnames(cmcExcitations);
 numMuscles = length(muscles)-1;
 f = figure('Name', 'Estimated EMG', 'WindowState', 'maximized');
 for i = 1:numMuscles
-    subplot(numMuscles,1,i, 'Parent',f);
+    subplot(ceil(numMuscles/2),2,i, 'Parent',f);
     hold on
     plot(cmcExcitations.time, cmcExcitations.(muscles{i+1}))
-    plot(cmcActivations.time, cmcActivations.(muscles{i+1}), 'Color', 'r')
-    title(muscles{i+1})
-    legend('Excitación', 'Activación')
+    if i<8
+        plot(cmcActivations.time, cmcActivations.(muscles{i+1}), 'Color', 'r')
+        legend('Excitación', 'Activación')
+    else
+        legend('Control')
+    end
+    title(replace(muscles{i+1}, '_', ' '))
     ylim([0 1])
     drawnow
 end
