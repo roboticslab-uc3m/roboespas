51cdcb03b22e94dd1c5e4a871941ce903b34dd2e
Angordil <43574001+Angordil@users.noreply.github.com>
2020-07-12 20:08:57 +0200

    Update Main.m

--- matlab/OpenSimControl/MatlabCode/Principales/Main.m
+++ matlab/OpenSimControl/MatlabCode/Principales/Main.m
@@ -638,6 +638,10 @@ rraTool.setLowpassCutoffFrequency(6);  %Se queja luego si no lo meto
 rraTool.setOutputModelFileName(strcat(CD_model,'\',strrep(MODELO, '.osim', '_adjusted.osim')));
 %Actuators
 %     rraTool.setForceSetFiles(strcat(CD_rra,'Reserve_Actuators_Roboespas.xml')); %%%%%%%
+% Look ahead window time
+rraTool.setTimeWindow(0.001);
+
+rraTool.setUseFastTarget(false);
 
 rraTool.setAdjustedCOMBody('thorax');
 rraTool.setAdjustCOMToReduceResiduals(1);
