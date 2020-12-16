a1b576d082b0ec10356cc46e659ebc75a804448b
Leytha <anac.jaquete@gmail.com>
2020-12-16 23:25:16 +0100

    Deleted unused files

--- RoboespasApp/MATLAB/IIWA/TestFRI/movej.m
+++
@@ -1,6 +0,0 @@
-init_ros;
-qini = IiwaCommandFRI.ReadCurrentJointPosition();
-qend = qini;
-qend(7) = qend(7)+deg2rad(20);
-IiwaCommandFRI.SetVelocity(0.1);
-[traj_theory, traj_comm, traj_read] = IiwaCommandFRI.MoveJ(qend);
